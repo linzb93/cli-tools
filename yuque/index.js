9527866527dc874971838f03f54f5f7bcb66354a
linzb <linzb93@qq.com>
2020-09-05 10:19:29 +0800

    新增语雀发布memo的代码

--- yuque/index.js
+++ yuque/index.js
@@ -1,3 +1,3 @@
-const {moduleInit} = require('../lib/util');
+const {subParser} = require('../lib/command-parser');
 
-module.exports = moduleInit.bind(this, true, __dirname);
+module.exports = subParser.bind(this, __dirname);
---
+++ yuque/memo.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const fs = require('fs-extra');
+const moment = require('moment');
+const path = require('path');
+const { errorLogger } = require('../lib/util');
+
+module.exports = async args => {
+    const BASEURL = 'https://www.yuque.com/api/v2';
+    let config;
+    try {
+        config = await fs.readJSON(path.join(__dirname, 'config.local.json'));
+    } catch (e) {
+        console.log('请先配置基础信息');
+        return;
+    }
+    const { token, name } = config;
+
+    const service = axios.create({
+        baseURL: BASEURL,
+        headers: {
+            'Content-Type': 'application/json',
+            'User-Agent': 'local',
+            'X-Auth-Token': token
+        }
+    });
+
+    try {
+        await service.post(`/repos/linzb93/diary/docs`, {
+            title: `memo ${moment().format('YYYY-MM-DD h:mm:ss a')}`,
+            body: args[0]
+        });
+    } catch (error) {
+        errorLogger(error.response.data);
+        return;   
+    }
+    console.log('推送成功');
+    // await service.put(`/repos/diary/docs/12703595`)
+}
