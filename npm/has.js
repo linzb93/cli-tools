8122c0c3f948a9951986eb58ef5fdafab4405d9b
linzb <linzb93@qq.com>
2020-10-16 17:02:06 +0800

    在npm包不存在的情况下，可以直接下载。

--- npm/has.js
+++ npm/has.js
@@ -1,11 +1,34 @@
 const fs = require('fs-extra');
-const {successLogger, errorLogger} = require('../lib/util');
+const inquirer = require('inquirer');
+const ora = require('ora');
+const execa = require('execa');
+const {successLogger} = require('../lib/util');
 
 module.exports = async args => {
-  let exists = await fs.exists(`./node_modules/${args[0]}`);
-  if (exists) {
-    successLogger(`${args[0]} 存在`);
-  } else {
-    errorLogger(`${args[0]} 不存在`);
+  const name = args[0];
+  try {
+    await fs.access(`./node_modules/${name}`, fs.constants.F_OK);
+  } catch (e) {
+    handleNotFound(name);
+    return;
+  }
+  successLogger(`${name} 存在`);
+}
+
+async function handleNotFound(name) {
+  const {action} = await inquirer.prompt({
+    type: 'confirm',
+    name: 'action',
+    message: `${name} 不存在，是否安装？`
+  });
+  if (action) {
+    const spinner = ora(`正在安装${name}`).start();
+    try {
+      await execa('cnpm', ['i', name, '-S']);
+    } catch (e) {
+      spinner.fail(`${name} 无法下载，请检查拼写是否有误。`);
+      return;
+    }
+    spinner.succeed(`${name} 安装成功`);
   }
 }
