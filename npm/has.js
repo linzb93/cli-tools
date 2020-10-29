0fb451ee765fbc72bd9c2ea0ab34c15298abce45
linzb <linzb93@qq.com>
2020-10-29 10:31:31 +0800

    npm包下载新增判断flag

--- npm/has.js
+++ npm/has.js
@@ -2,29 +2,34 @@ const fs = require('fs-extra');
 const inquirer = require('inquirer');
 const ora = require('ora');
 const execa = require('execa');
-const {successLogger} = require('../lib/util');
+const {successLogger, errorLogger} = require('../lib/util');
 
-module.exports = async args => {
+module.exports = async (args, flag) => {
   const name = args[0];
   try {
     await fs.access(`./node_modules/${name}`, fs.constants.F_OK);
   } catch (e) {
-    handleNotFound(name);
+    handleNotFound(name, flag);
     return;
   }
   successLogger(`${name} 存在`);
 }
 
-async function handleNotFound(name) {
+async function handleNotFound(name, flag) {
   const {action} = await inquirer.prompt({
     type: 'confirm',
     name: 'action',
     message: `${name} 不存在，是否安装？`
   });
+  const flags = ['S', 'D'];
+  if (!flags.includes(flag)) {
+    errorLogger(`flag有误，应该是${flags.map(flagItem => `-${flagItem}`).join('或')}`);
+    return;
+  }
   if (action) {
     const spinner = ora(`正在安装${name}`).start();
     try {
-      await execa('cnpm', ['i', name, '-S']);
+      await execa('cnpm', ['i', name, `-${flag}`]);
     } catch (e) {
       spinner.fail(`${name} 无法下载，请检查拼写是否有误。`);
       return;
