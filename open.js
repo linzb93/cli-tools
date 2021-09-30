b3661c1c126e1898bd0944dc51c22d9cf8721738
linzb <linzb93@qq.com>
2021-09-30 17:41:08 +0800

    update

--- open.js
+++ open.js
@@ -5,35 +5,25 @@ const logger = require('./lib/logger');
 const fs = require('fs-extra');
 const path = require('path');
 const {openInEditor} = require('./lib/util');
-const {clidb} = require('./lib/db');
-module.exports = async (name, option) => {
-    if (!option.code) {
-        const map = await clidb.get('openMap');
-        if (map && map[name]) {
-            open(map[name]);
-        } else {
-            logger.error('目录不存在，请重新输入');
-        }
+module.exports = async (name) => {
+    if (name === 'source') {
+        const sourceDir = await clidb.get('sourceCodeDir');
+        const dirs = await fs.readdir(sourceDir);
+        const {source} = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'source',
+                message: '选择要打开的源码',
+                choices: dirs.map(dir => path.basename(dir))
+            }
+        ]);
+        await openInEditor(path.join(sourceDir, source));
+        return;
+    }
+    const code = await clidb.get('code');
+    if (code[name]) {
+        await openInEditor(code[name]);
     } else {
-        if (name === 'source') {
-            const sourceDir = await clidb.get('sourceCodeDir');
-            const dirs = await fs.readdir(sourceDir);
-            const {source} = await inquirer.prompt([
-                {
-                    type: 'list',
-                    name: 'source',
-                    message: '选择要打开的源码',
-                    choices: dirs.map(dir => path.basename(dir))
-                }
-            ]);
-            await openInEditor(path.join(sourceDir, source));
-            return;
-        }
-        const code = await clidb.get('code');
-        if (code[name]) {
-            await openInEditor(code[name]);
-        } else {
-            logger.error('项目不存在');
-        }
+        logger.error('项目不存在');
     }
 }
