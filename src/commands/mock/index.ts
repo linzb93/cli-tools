0242d82ced09d63d5dfd7346c668ce1a1486abd2
林志斌 <linzb93@qq.com>
2023-03-07 20:27:03 +0800

    update

--- src/commands/mock/index.ts
+++ src/commands/mock/index.ts
@@ -2,22 +2,36 @@ import BaseCommand from '../../util/BaseCommand.js';
 import set from './set.js';
 import chalk from 'chalk';
 import axios from 'axios';
+import { fork } from 'child_process';
 import lodash from 'lodash';
-const { flatten } = lodash;
+import clipboardy from 'clipboardy';
+import { reactive } from '@vue/reactivity';
+import { watch } from '@vue/runtime-core';
 import pMap from 'p-map';
+import path from 'path';
+
+const { flatten } = lodash;
 const service = axios.create({
   baseURL: 'http://192.168.0.107:3000/api'
 });
 service.interceptors.response.use(
-  (res) => res.data.data,
+  (res) => {
+    if (res.data.errcode) {
+      return Promise.reject(res.data.errcode);
+    }
+    return res.data.data;
+  },
   (err) => err
 );
 
 class Mock extends BaseCommand {
   action: string;
+  private cookie: string;
   constructor(action: string) {
     super();
     this.action = action;
+    this.cookie = '';
+    this.genCooie();
   }
   async run() {
     if (this.action === 'set') {
@@ -27,14 +41,20 @@ class Mock extends BaseCommand {
     let db = this.helper.createDB('yapi');
     await db.read();
     let answer = {
-      project: ''
+      project: '',
+      prefix: '',
+      id: ''
     };
-    if ((db.data as any).item.length === 0) {
+    if ((db.data as any).items.length === 0) {
       this.logger.error('没有项目可以选择');
       return;
     }
     if ((db.data as any).items.length === 1) {
-      answer = (db.data as any).items[0];
+      const target = (db.data as any).items[0];
+      answer = {
+        ...target,
+        project: target.id
+      };
     } else {
       answer = (await this.helper.inquirer.prompt({
         message: '请选择项目',
@@ -44,52 +64,135 @@ class Mock extends BaseCommand {
           name: `${chalk.yellow(item.name)}: ${chalk.green(item.prefix)}`
         })),
         name: 'project'
-      })) as { project: string };
+      })) as { project: string; id: string; prefix: string };
+    }
+    if (this.action === 'debug') {
+      await this.createServer(answer);
+      return;
     }
     // 同步api
-    const headerCookie = `_yapi_uid=${this.ls.get(
-      'mock.uid'
-    )}; _yapi_token=${this.ls.get('mock.token')}`;
-    const res = (await service({
-      method: 'GET',
-      url: `/interface/list_menu`,
-      params: {
-        // eslint-disable-next-line camelcase
-        project_id: answer.project
-      },
-      headers: {
-        Cookie: headerCookie
-      }
-    })) as any;
+    const list = await this.getApiList(answer.project);
     db = this.helper.createDB(`yapi.${answer.project}`);
     await db.read();
     const result = flatten(
-      res.map((item: any) => {
+      list.map((item: any) => {
         return item.list.map((sub: any) => ({
           path: sub.path,
           title: sub.title,
-          updateTime: sub.up_time
+          updateTime: sub.up_time,
+          id: sub._id
         }));
       })
     );
     const source = (db.data as any).items;
-    await pMap(
+    const counter = reactive({
+      add: 0,
+      update: 0,
+      total: 0
+    });
+    watch(counter, (data) => {
+      this.spinner.text = `已扫描${chalk.green(
+        data.total
+      )}个，其中新增${chalk.green(data.add)}个，更新${chalk.green(
+        data.update
+      )}个`;
+    });
+    (db.data as any).items = await pMap(
       result,
       async (item: any) => {
         const match = source.find((s: any) => s.id === item.id);
-        if (!match || match.updateTime < item.up_time) {
-          await this.updateMock(match.id);
+        counter.total++;
+        if (!match) {
+          counter.add++;
+          return {
+            ...item,
+            json: await this.updateMock(item.id)
+          };
         }
+        if (match.updateTime < item.up_time) {
+          counter.update++;
+          return {
+            ...item,
+            json: await this.updateMock(item.id)
+          };
+        }
+        return match;
       },
       { concurrency: 10 }
     );
-    (db.data as any).items = result;
     await db.write();
+    this.spinner.succeed();
+    await this.helper.sleep(1500);
+    await this.createServer(answer);
   }
-  async updateMock(id: number) {
-    const headerCookie = `_yapi_uid=${this.ls.get(
+  private async createServer({ prefix, id }: { prefix: string; id: string }) {
+    const child = fork(
+      path.resolve(this.helper.root, 'dist/commands/mock/server.js'),
+      [`--prefix=${prefix}`, `--id=${id}`],
+      {
+        cwd: this.helper.root,
+        detached: true,
+        stdio: [null, null, null, 'ipc']
+      }
+    );
+    child.on(
+      'message',
+      async ({
+        port,
+        ip,
+        info
+      }: {
+        port: string;
+        ip: string;
+        info?: string;
+      }) => {
+        console.log(`info:${info}`);
+        console.log(`代理服务器已在 ${chalk.yellow(port)} 端口启动：
+      - 本地：${chalk.magenta(`http://localhost:${port}${prefix}`)}
+      - 网络：${chalk.magenta(`http://${ip}:${port}${prefix}`)}
+      `);
+        child.unref();
+        child.disconnect();
+        process.exit(0);
+      }
+    );
+  }
+  private genCooie() {
+    this.cookie = `_yapi_uid=${this.ls.get(
       'mock.uid'
     )}; _yapi_token=${this.ls.get('mock.token')}`;
+  }
+  async getApiList(id: string): Promise<any[]> {
+    try {
+      return (await service({
+        method: 'GET',
+        url: `/interface/list_menu`,
+        params: {
+          // eslint-disable-next-line camelcase
+          project_id: id
+        },
+        headers: {
+          Cookie: this.cookie
+        }
+      })) as any;
+    } catch (error) {
+      await this.login();
+      return await this.getApiList(id);
+    }
+  }
+  async login() {
+    const { is } = await this.helper.inquirer.prompt({
+      type: 'confirm',
+      name: 'is',
+      message: 'token已过期，确认使用剪贴板的内容吗？'
+    });
+    if (!is) {
+      process.exit(0);
+    }
+    this.ls.set('mock.token', clipboardy.readSync());
+    this.genCooie();
+  }
+  async updateMock(id: number) {
     const res = (await service({
       method: 'GET',
       url: `/interface/get`,
@@ -97,11 +200,15 @@ class Mock extends BaseCommand {
         id
       },
       headers: {
-        Cookie: headerCookie
+        Cookie: this.cookie
       }
     })) as any;
-    const responseBody = JSON.parse(res.res_body);
-    render(responseBody);
+    if (res.res_body) {
+      const responseBody = JSON.parse(res.res_body);
+      return render(responseBody);
+    } else {
+      return {};
+    }
   }
 }
 
@@ -122,8 +229,8 @@ function render(src: any) {
     } else if (isTime(key)) {
       ret[key] = `@date(YYYY-MM-DD HH:mm:ss)`;
     } else if (isId(key)) {
-      ret[`${key}|+1`] = `@id`;
-    } else if (isEnum(p[key].description)) {
+      ret[`${key}|+1`] = 1;
+    } else if (isEnum(p[key])) {
       ret[`${key}|1`] = getEnum(p[key].description);
     } else if (p[key].type === 'string') {
       ret[key] = '@title(5)';
@@ -164,7 +271,7 @@ function isTime(key: string) {
 }
 
 function isEnum(object: any): boolean {
-  const { description } = object;
+  const { description = '' } = object;
   return isEnumInclude(description, '0,1') || isEnumInclude(description, '1,2');
 }
 
---
+++ src/commands/mock/server.ts
@@ -0,0 +1,48 @@
+import { Command } from 'commander';
+import express from 'express';
+import fs from 'fs-extra';
+import cors from 'cors';
+import getPort from 'detect-port';
+import internalIp from 'internal-ip';
+import path from 'path';
+import * as helper from '../../util/helper.js';
+import Mock from 'mockjs';
+const program = new Command();
+
+program
+  .option('--prefix <prefix>', '地址')
+  .option('--id <id>', '项目ID')
+  .allowUnknownOption()
+  .action(async (_, optArg) => {
+    const options = optArg.opts ? optArg.opts() : optArg;
+    const app = express();
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(cors());
+    app.all(`${options.prefix}/*`, async (req, res) => {
+      const map = await fs.readJSON(
+        path.resolve(helper.root, `data/yapi/${options.id}.json`)
+      );
+      const url = req.url.replace(options.prefix, '');
+      await fs.writeFile('debug.txt', `debug:${123}\n`);
+      const match = map.items.find((item: any) => item.path === url);
+      if (!match) {
+        res.send({
+          code: 404,
+          data: null,
+          msg: '接口不存在',
+          result: null
+        });
+        return;
+      }
+      res.send(Mock.mock(match.json));
+    });
+    const [port, ip] = await Promise.all([
+      getPort(options.port || 8080),
+      internalIp.v4()
+    ]);
+    app.listen(port, () => {
+      process.send?.({ port, ip, info: options.prefix });
+    });
+  });
+program.parse(process.argv);
