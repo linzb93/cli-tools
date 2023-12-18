d3d252403ce6e8e0e30b98f503eb8aa0eb77aa77
林志斌 <linzb93@qq.com>
2023-12-18 15:22:17 +0800

    update

--- src/commands/server/index.ts
+++ src/commands/server/index.ts
@@ -4,6 +4,7 @@ import notifier from 'node-notifier';
 import bodyParser from 'body-parser';
 import chalk from 'chalk';
 import { get as getIp } from '../ip.js';
+import path from 'path';
 import fs from 'fs-extra';
 class Server extends BaseCommand {
   run() {
@@ -26,7 +27,9 @@ class Server extends BaseCommand {
       const app = express();
       app.use(bodyParser.urlencoded({ extended: false }));
       app.use(bodyParser.json({ limit: '5mb' }));
-      const tasks = await fs.readdir(`./dist/commands/server/tasks`);
+      const tasks = await fs.readdir(
+        path.resolve(this.helper.root, `./dist/commands/server/tasks`)
+      );
       const services: any[] = [];
       for (const task of tasks) {
         const obj = (await import(`./tasks/${task}`)).default;
@@ -46,8 +49,8 @@ class Server extends BaseCommand {
 服务器已在端口6060开启,地址是${chalk.yellow(`${ip}:6060`)}
 下列服务已加载：
 ${services
-    .map((service, index) => chalk.magenta(`${index + 1}.${service}`))
-    .join('\n')}
+  .map((service, index) => chalk.magenta(`${index + 1}.${service}`))
+  .join('\n')}
         `);
       });
     });
