f84db9cf8a74fa3ab2782728d6b7a5fc10764e1f
林志斌 <linzb93@qq.com>
2024-05-10 14:39:40 +0800

    feat:适配windows系统的vue项目启动

--- src/commands/ipc.ts
+++ src/commands/ipc.ts
@@ -1,6 +1,9 @@
 import BaseCommand from '../util/BaseCommand.js';
 import ipc from 'node-ipc';
 import { execaCommand as execa } from 'execa';
+
+const isMac = process.platform === 'darwin';
+
 class Ipc extends BaseCommand {
   async run() {
     // 目前在Windows系统提供给Electron app启动vue项目
@@ -10,7 +13,7 @@ class Ipc extends BaseCommand {
       ipc.server.on('message', async (data, socket) => {
         const { requestId } = data;
         if (data.action === 'vue-build') {
-          await execa('npm run build', {
+          await execa('npx vue-cli-service build', {
             cwd: data.params.cwd
           });
           ipc.server.emit(socket, 'response', {
@@ -18,18 +21,35 @@ class Ipc extends BaseCommand {
             requestId
           });
         } else if (data.action === 'vue-serve') {
-          const std = execa('npm run serve', {
+          const std = execa('npx vue-cli-service serve', {
             cwd: data.params.cwd
           });
-          std.stdout?.on('data', (message) => {
-            if (message.toString().includes('- Network')) {
-              ipc.server.emit(socket, 'response', {
-                code: 200,
-                message: message.toString(),
-                requestId
-              });
-            }
-          });
+          const IpPortReg = /(\d{1,3}\.){3}\d{1,3}:\d{4}/;
+          if (isMac) {
+            std.stdout?.on('data', (message) => {
+              if (message.toString().includes('- Network')) {
+                const arr = message.toString().match(IpPortReg);
+                ipc.server.emit(socket, 'response', {
+                  code: 200,
+                  address: arr ? arr[0] : '',
+                  requestId
+                });
+              }
+            });
+          } else {
+            // 最好的办法是3秒以内没有收到信息才emit
+            std.stderr?.on('data', async (message) => {
+              const arr = message.toString().match(IpPortReg);
+              if (arr) {
+                await this.helper.sleep(3000);
+                ipc.server.emit(socket, 'response', {
+                  code: 200,
+                  address: arr[0],
+                  requestId
+                });
+              }
+            });
+          }
         }
       });
     });
