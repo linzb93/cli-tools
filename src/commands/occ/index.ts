0707f1b30616690fcaa7b744c053948b6fc9378c
L IN <linzb93@users.noreply.github.com>
2024-05-21 21:18:26 +0800

    chore:修改ts配置

--- src/commands/occ/index.ts
+++ src/commands/occ/index.ts
@@ -1,12 +1,11 @@
-import axios, { AxiosInstance } from 'axios';
-import open from 'open';
-import clipboard from 'clipboardy';
-import BaseCommand from '../../util/BaseCommand.js';
-import login from './login.js';
-import { stringify } from 'querystring';
-import chalk from 'chalk';
-import map from './sitemap.js';
-import { MeituanLoginParams, EleLoginParams } from './types';
+import axios, { AxiosInstance } from "axios";
+import open from "open";
+import clipboard from "clipboardy";
+import BaseCommand from "../../util/BaseCommand.js";
+import { stringify } from "querystring";
+import chalk from "chalk";
+import map from "./sitemap.js";
+import { MeituanLoginParams, EleLoginParams } from "./types";
 interface Options {
   token: string | boolean;
   pc: boolean;
@@ -35,25 +34,25 @@ class OCC extends BaseCommand {
   constructor(private input: string[], private options: Options) {
     super();
     this.currentApp = map.default;
-    this.memberId = '';
+    this.memberId = "";
     this.service = axios.create({
-      baseURL: ''
+      baseURL: "",
     });
   }
   async run() {
     const { options } = this;
     this.setMatchProject();
-    if (this.currentApp.name === 'chain') {
+    if (this.currentApp.name === "chain") {
       this.handleChainProject();
       return;
     }
-    if (this.currentApp.name === 'sg') {
+    if (this.currentApp.name === "sg") {
       this.handleSgProject();
       return;
     }
     const { shop, url } = await this.getShop();
     const shopName = (() => {
-      if (this.currentApp.name === 'ele') {
+      if (this.currentApp.name === "ele") {
         return shop.shopName || shop.shopId;
       }
       return shop.memberName || shop.memberId;
@@ -61,8 +60,8 @@ class OCC extends BaseCommand {
     if (options.token === true) {
       // token无输入值，就只是复制token
       const { hash } = new URL(url);
-      const fullToken = hash.replace('#/login?code=', '');
-      const token = fullToken.replace(/occ_(senior_)?/, '').replace(/&.+/, '');
+      const fullToken = hash.replace("#/login?code=", "");
+      const token = fullToken.replace(/occ_(senior_)?/, "").replace(/&.+/, "");
       clipboard.writeSync(token);
       this.spinner.succeed(
         `【${this.currentApp.serviceName}】已复制店铺 ${shopName} 的token\n${token}`
@@ -70,7 +69,7 @@ class OCC extends BaseCommand {
     } else if (options.token) {
       // token有值，就是根据token登录
       const { origin, pathname } = new URL(url);
-      this.spinner.succeed('打开成功');
+      this.spinner.succeed("打开成功");
       open(`${origin}${pathname}#/login?code=${options.token}`);
     } else if (options.copy) {
       clipboard.writeSync(url);
@@ -79,15 +78,15 @@ class OCC extends BaseCommand {
       );
     } else if (options.user) {
       const { hash } = new URL(url);
-      const token = hash.replace('#/login?code=', '');
-      this.spinner.text = '正在获取用户信息';
+      const token = hash.replace("#/login?code=", "");
+      this.spinner.text = "正在获取用户信息";
       const { data } = await axios.post(
-        this.ls.get('oa.userApiPrefix') + this.currentApp.url.userApi,
+        this.ls.get("oa.userApiPrefix") + this.currentApp.url.userApi,
         {},
         {
           headers: {
-            token
-          }
+            token,
+          },
         }
       );
       this.spinner.succeed(`获取店铺 ${shopName} 信息成功`);
@@ -95,15 +94,15 @@ class OCC extends BaseCommand {
     } else {
       if (
         options.pc &&
-        ['4', '73', '36', '75'].includes(this.currentApp.appKey)
+        ["4", "73", "36", "75"].includes(this.currentApp.appKey)
       ) {
         // 只有美团经营神器、装修神器、评价神器和IM神器有PC端
-        open(url.replace('app', ''));
+        open(url.replace("app", ""));
       } else {
         open(url);
       }
       const shopName = (() => {
-        if (this.currentApp.name === 'ele') {
+        if (this.currentApp.name === "ele") {
           return shop.shopName || shop.shopId;
         }
         return shop.memberName || shop.memberId;
@@ -125,47 +124,46 @@ class OCC extends BaseCommand {
       platform: match.platform,
       version: 1,
       minPrice: 0,
-      serviceName: match.serviceName
+      serviceName: match.serviceName,
     };
     let listData: ShopListResponse;
     this.logger.debug(listSearchParams);
     try {
       const res = await service.post(match.url.list, listSearchParams, {
         headers: {
-          token: this.ls.get('oa.token')
-        }
+          token: this.ls.get("oa.token"),
+        },
       });
       listData = res.data;
     } catch (error) {
       this.spinner.fail(
         this.helper.showWeakenTips(
-          '服务器故障，请稍后再试。',
+          "服务器故障，请稍后再试。",
           (error as Error).message
         )
       );
       process.exit(1);
     }
     if (listData.code === 401) {
-      await login(this.options);
       return await this.getSearchList();
     } else if (!listData.result) {
       this.spinner.fail(
         this.helper.showWeakenTips(
-          '服务器故障，请稍后再试。',
+          "服务器故障，请稍后再试。",
           JSON.stringify(listData)
         )
       );
     } else if (!listData.result.list.length) {
       this.logger.debug(listData);
-      this.spinner.fail('未找到店铺', true);
+      this.spinner.fail("未找到店铺", true);
     }
     return listData.result.list;
   }
   private setMatchProject(): void {
     const { input } = this;
     let match = {} as typeof map.default;
-    let memberId = '';
-    this.spinner.text = '正在搜索店铺';
+    let memberId = "";
+    this.spinner.text = "正在搜索店铺";
     if (input.length === 0) {
       match = map.default;
       memberId = !this.options.test ? match.defaultId : match.testId;
@@ -173,7 +171,7 @@ class OCC extends BaseCommand {
       if (isNaN(Number(input[0])) && this.helper.isValidKey(input[0], map)) {
         match = map[input[0]];
         if (!match) {
-          this.spinner.fail('项目不存在，请重新输入', true);
+          this.spinner.fail("项目不存在，请重新输入", true);
         }
         memberId = !this.options.test ? match.defaultId : match.testId;
       } else {
@@ -184,13 +182,13 @@ class OCC extends BaseCommand {
       if (isNaN(Number(input[0])) && this.helper.isValidKey(input[0], map)) {
         match = map[input[0]];
         if (!match) {
-          this.spinner.fail('项目不存在，请重新输入', true);
+          this.spinner.fail("项目不存在，请重新输入", true);
         }
         memberId = input[1];
       } else if (this.helper.isValidKey(input[1], map)) {
         match = map[input[1]];
         if (!match) {
-          this.spinner.fail('项目不存在，请重新输入', true);
+          this.spinner.fail("项目不存在，请重新输入", true);
         }
         memberId = input[0];
       }
@@ -200,12 +198,12 @@ class OCC extends BaseCommand {
     this.service = axios.create({
       baseURL: `${
         this.options.test
-          ? this.ls.get('oa.testPrefix')
-          : this.ls.get('oa.apiPrefix')
+          ? this.ls.get("oa.testPrefix")
+          : this.ls.get("oa.apiPrefix")
       }${match.url.base}`,
       headers: {
-        token: this.ls.get('oa.token')
-      }
+        token: this.ls.get("oa.token"),
+      },
     });
   }
   private async getShop(): Promise<{
@@ -216,7 +214,7 @@ class OCC extends BaseCommand {
     const list = await this.getSearchList();
     const shop = list[0] as ShopItem;
     const shopName = (() => {
-      if (match.name === 'ele') {
+      if (match.name === "ele") {
         return shop.shopName || shop.shopId;
       }
       return shop.memberName || shop.memberId;
@@ -228,15 +226,15 @@ class OCC extends BaseCommand {
     }
     await this.helper.sleep(1500);
     const {
-      data: { result }
+      data: { result },
     } = await this.service.post(match.url.login, match.loginKey(shop), {
       headers: {
-        token: this.ls.get('oa.token')
-      }
+        token: this.ls.get("oa.token"),
+      },
     });
     return {
       url: result,
-      shop
+      shop,
     };
   }
   private async handleChainProject() {
@@ -255,29 +253,29 @@ class OCC extends BaseCommand {
     })();
     axios
       .post(
-        `${this.ls.get('oa.oldApiPrefix')}/chain/occ/dkdAccount/oa/listAccount`,
+        `${this.ls.get("oa.oldApiPrefix")}/chain/occ/dkdAccount/oa/listAccount`,
         {
           searchParam,
-          accountStatus: '',
+          accountStatus: "",
           pageSize: 1,
-          pageIndex: 1
+          pageIndex: 1,
         }
       )
       .then((response) => {
         const target = response.data.result.list[0];
         if (!target) {
-          this.spinner.fail('店铺不存在', true);
+          this.spinner.fail("店铺不存在", true);
         }
         return Promise.all([
           axios.post(
             `${this.ls.get(
-              'oa.oldApiPrefix'
+              "oa.oldApiPrefix"
             )}/chain/occ/dkdAccount/oa/getAccountToken`,
             {
-              id: target.id
+              id: target.id,
             }
           ),
-          Promise.resolve(target.brandName)
+          Promise.resolve(target.brandName),
         ]);
       })
       .then(([response, brandName]) => {
@@ -301,7 +299,7 @@ class OCC extends BaseCommand {
               createTime,
               phoneNumber,
               id,
-              shopNumber
+              shopNumber,
             })}`
           );
         }
@@ -315,7 +313,7 @@ class OCC extends BaseCommand {
     const { memberId } = this;
     this.logger.debug(`memberId:${memberId}`);
     const { options } = this;
-    const token = this.ls.get('oa.token');
+    const token = this.ls.get("oa.token");
     const queryParam = (() => {
       if (memberId) {
         return memberId;
@@ -326,36 +324,35 @@ class OCC extends BaseCommand {
       return match.testId;
     })();
     const response = await axios.post(
-      `${this.ls.get('oa.apiPrefix')}/retailManage/pageQueryOccOrder`,
+      `${this.ls.get("oa.apiPrefix")}/retailManage/pageQueryOccOrder`,
       {
         queryParam,
         pageSize: 1,
-        pageIndex: 1
+        pageIndex: 1,
       },
       {
         headers: {
-          token
-        }
+          token,
+        },
       }
     );
     if (response.data.code === 401) {
-      await login(this.options);
       return await this.handleChainProject();
     }
     const target = response.data.result.list[0];
     if (!target) {
-      this.spinner.fail('店铺不存在', true);
+      this.spinner.fail("店铺不存在", true);
     }
     const tokenResponse = await axios.post(
-      `${this.ls.get('oa.apiPrefix')}/retailManage/getShopTokenToUse`,
+      `${this.ls.get("oa.apiPrefix")}/retailManage/getShopTokenToUse`,
       {
         memberId: target.memberId,
-        appKey: match.appKey
+        appKey: match.appKey,
       },
       {
         headers: {
-          token
-        }
+          token,
+        },
       }
     );
     const { occToken } = tokenResponse.data.result;
