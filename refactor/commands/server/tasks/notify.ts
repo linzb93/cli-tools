c6d5b5649b879f6632be7a200a7880c2d874f9c8
L IN <linzb93@users.noreply.github.com>
2024-05-23 22:15:40 +0800

    refactor:删除无用的代码

--- refactor/commands/server/tasks/notify.ts
+++
@@ -1,9 +0,0 @@
-type Notify = (content: string) => void;
-export default {
-  loaded: true,
-  name: '测试通知功能',
-  executeTime: '11:47',
-  actions: async ({ notify, params }: { notify: Notify; params: any }) => {
-    notify(JSON.stringify(params));
-  }
-};
