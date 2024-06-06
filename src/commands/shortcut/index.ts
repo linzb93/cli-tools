4c3927423c72d2d73c9e7bc6d3231d3fc3f1d02c
林志斌 <linzb93@qq.com>
2024-06-06 20:10:49 +0800

    refactor:优化shortcut

--- src/commands/shortcut/index.ts
+++ src/commands/shortcut/index.ts
@@ -7,77 +7,83 @@ class Shortcut extends BaseCommand {
   run() {
     const map = [
       {
-        title: "聚焦文件资源管理器（自定义）",
-        windows: "alt + `",
-        mac: "",
+        type: 'common',
+        children: [
+          {
+            title: "切换至下一个页面",
+            windows: "ctrl + page down",
+            mac: "shift + cmd + ]",
+          },
+          {
+            title: "切换至上一个页面",
+            windows: "ctrl + page up",
+            mac: "shift + cmd + [",
+          },
+        ]
       },
       {
-        title: "切换至下一个文件",
-        windows: "ctrl + page down",
-        mac: "shift + cmd + ]",
+        type: 'vscode',
+        children: [
+          {
+            title: "聚焦文件资源管理器（自定义）",
+            windows: "alt + `",
+            mac: "",
+          },
+          {
+            title: "新建文件",
+            windows: "ctrl + alt + n",
+            mac: "",
+          },
+          {
+            title: "新建文件夹",
+            windows: "ctrl + shift + alt + n",
+            mac: "",
+          },
+          {
+            title: "聚焦终端",
+            windows: "ctrl + j",
+            mac: "",
+          },
+          {
+            title: "新建终端",
+            windows: "ctrl + shift + `",
+            mac: "",
+          },
+          {
+            title: "清除终端内容",
+            windows: "ctrl + k",
+            mac: "",
+          },
+        ],
       },
       {
-        title: "切换至上一个文件",
-        windows: "ctrl + page up",
-        mac: "shift + cmd + [",
+        type: 'chrome',
+        children: [
+          {
+            title: "切换到下一个标签",
+            windows: "",
+            mac: "command + option + ->",
+          },
+        ]
       },
-      {
-        title: "新建文件",
-        windows: "ctrl + alt + n",
-        mac: "",
-      },
-      {
-        title: "新建文件夹",
-        windows: "ctrl + shift + alt + n",
-        mac: "",
-      },
-      {
-        title: "聚焦终端",
-        windows: "ctrl + j",
-        mac: "",
-      },
-      {
-        title: "新建终端",
-        windows: "ctrl + shift + `",
-        mac: "",
-      },
-      {
-        title: "聚焦上一个终端",
-        windows: "ctrl + page up",
-        mac: "",
-      },
-      {
-        title: "聚焦下一个终端",
-        windows: "ctrl + page down",
-        mac: "",
-      },
-      {
-        title: "清除终端内容",
-        windows: "ctrl + k",
-        mac: "",
-      },
-      {
-        title: "chrome切换到下一个标签",
-        windows: "",
-        mac: "command + option + ->",
-      },
+      
     ];
-    const list = this.name
-      ? map.filter((item) => item.title.includes(this.name))
-      : map;
-    if (!list.length) {
-      this.logger.error("没有找到对应的命令");
+    if (!this.name) {
+      console.log(map.map(panel => {
+        return `${chalk.bold.red(panel.type)}
+  ${panel.children.map(item => `${chalk.blue(item.title)} ${this.helper.isWin ? item.windows : item.mac}`).join('\n  ')}`
+      }).join('\n'))
     } else {
-      console.log(
-        list
-          .map(
-            (item) =>
-              `${chalk.cyan(item.title)} ${
-                this.helper.isWin ? item.windows : item.mac
-              }`
-          )
-          .join("\n")
-      );
+      if (!['chrome', 'vscode'].includes(this.name)) {
+        this.logger.error('不存在的平台，只支持vscode和chrome');
+      } else {
+        const list = [
+          map[0],
+          map.find(panel => panel.type === this.name)
+        ];
+        const flatList = list.reduce((acc, panel) => acc.concat(panel.children), [])
+        console.log(flatList.map(item => `${chalk.blue(item.title)} ${this.helper.isWin ? item.windows : item.mac}`).join('\n'));
+      }
     }
   }
 }
