df55565b09fae1543712f8e3d0ad605c5b503173
林志斌 <linzb93@qq.com>
2024-08-22 18:05:33 +0800

    feat:移除python打包生成的无用文件

--- packages/exe/index.js
+++ packages/exe/index.js
@@ -7,10 +7,11 @@ const pythonDirs = join(fileURLToPath(import.meta.url), "../python");
 const targetDir = resolve(pythonDirs, "../../server/dist/lib");
 const dirs = await fs.readdir(pythonDirs);
 await pMap(dirs, async (file) => {
+  const execName = file.replace(/\.py$/, "");
   await execa(
-    `pyinstaller --onefile ${pythonDirs}/${file} --distpath=${targetDir}`,
-    {
-      stdout: process.stdout,
-    }
+    `pyinstaller --onefile ${pythonDirs}/${file} --distpath=${targetDir}`
   );
+  // 移除无用的产物
+  await fs.remove(`build/${execName}`);
+  await fs.unlink(`${execName}.spec`);
 });
