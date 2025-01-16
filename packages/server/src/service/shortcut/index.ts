fc0f79596e5fba66cb674e73617e7a26e895a9e8
林志斌 <linzb93@qq.com>
2025-01-16 14:29:46 +0800

    feat:快捷键搜索支持名称模糊搜索

--- packages/server/src/service/shortcut/index.ts
+++ packages/server/src/service/shortcut/index.ts
@@ -1,12 +1,13 @@
 import BaseCommand from '@/common/BaseCommand';
 import chalk from 'chalk';
 import { isWin } from '@/common/constant';
+import { groupBy } from 'lodash-es';
 
 interface IShortcutItem {
     title: string;
     windows: string;
     mac: string;
-    type?: 'vscode' | 'chrome';
+    type: 'all' | 'vscode' | 'chrome';
 }
 
 export default class Shortcut extends BaseCommand {
@@ -19,31 +20,37 @@ export default class Shortcut extends BaseCommand {
                 title: '切换至下一个页面',
                 windows: 'ctrl + page down',
                 mac: 'shift + cmd + ]',
+                type: 'all',
             },
             {
                 title: '切换至上一个页面',
                 windows: 'ctrl + page up',
                 mac: 'shift + cmd + [',
+                type: 'all',
             },
             {
                 title: '窗口最大化',
                 windows: 'windows + up',
                 mac: '无',
+                type: 'all',
             },
             {
                 title: '关闭窗口',
                 windows: 'shift + ctrl + w',
                 mac: 'shift + cmd + w',
+                type: 'all',
             },
             {
                 title: '主副屏切换',
-                windows: 'shift + alt + right',
+                windows: 'shift + window + right',
                 mac: '无',
+                type: 'all',
             },
             {
                 title: '截图',
                 windows: 'ctrl + a',
                 mac: 'shift + command + 2',
+                type: 'all',
             },
 
             {
@@ -119,21 +126,40 @@ export default class Shortcut extends BaseCommand {
                 type: 'chrome',
             },
         ];
+        const grouped = groupBy(shortcutMap, 'type') as {
+            all: IShortcutItem[];
+            chrome: IShortcutItem[];
+            vscode: IShortcutItem[];
+        };
         if (!this.name) {
-            console.log();
+            const data = Object.keys(grouped) as (keyof typeof grouped)[];
             console.log(
-                shortcutMap.map((item) => `${chalk.blue(item.title)} ${isWin ? item.windows : item.mac}`).join('\n  ')
+                data
+                    .map((key) => {
+                        return `${chalk.bold.red(key)}
+${chalk.red(`-------------------`)}
+${grouped[key].map((item) => this.renderItem(item)).join('\n')}`;
+                    })
+                    .join('\n\n')
             );
-        } else {
-            if (!['chrome', 'vscode'].includes(this.name)) {
-                this.logger.error('不存在的平台，只支持vscode和chrome');
-            } else {
-                // const list = [map[0], map.find((panel) => panel.type === this.name)];
-                // const flatList = list.reduce((acc, panel) => acc.concat(panel.children), []);
-                // console.log(
-                //     flatList.map((item) => `${chalk.blue(item.title)} ${isWin ? item.windows : item.mac}`).join('\n')
-                // );
+        } else if (['chrome', 'vscode'].includes(this.name)) {
+            let arr = [];
+            if (this.name === 'vscode') {
+                arr = [...grouped.all, ...grouped.vscode];
+            } else if (this.name === 'chrome') {
+                arr = [...grouped.all, ...grouped.chrome];
             }
+            console.log(arr.map((item) => this.renderItem(item)).join('\n'));
+        } else {
+            const filters = shortcutMap.filter((item) => item.title.includes(this.name));
+            if (!filters.length) {
+                this.logger.info('没找到快捷键');
+                return;
+            }
+            console.log(filters.map((item) => this.renderItem(item)).join('\n'));
         }
+    }
+    private renderItem(item: IShortcutItem) {
+        return `${chalk.cyan(item.title)} ${isWin ? item.windows : item.mac}`;
     }
 }
