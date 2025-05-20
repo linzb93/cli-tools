296e6fe7f665c60b252ffa190d6bb9295a1f4064
L IN <linzb93@users.noreply.github.com>
2025-05-20 22:53:22 +0800

    feat:屏蔽yapi

--- packages/server/src/cli/bin.ts
+++ packages/server/src/cli/bin.ts
@@ -14,7 +14,7 @@ import color from './commands/color';
 import fork from './commands/fork';
 import kill from './commands/kill';
 import lixi from './commands/lixi';
-import yapi from './commands/yapi';
+// import yapi from './commands/yapi';
 // import mock from "./commands/mock";
 import shortcut from './commands/shortcut';
 import getSize from './commands/size';
@@ -239,11 +239,11 @@ program
     .action((option) => {
         vue(option);
     });
-program
-    .command('yapi <url>')
-    .description('获取yapi接口文档')
-    .action((url) => {
-        yapi(url);
-    });
+// program
+//     .command('yapi <url>')
+//     .description('获取yapi接口文档')
+//     .action((url) => {
+//         yapi(url);
+//     });
 
 program.parse(process.argv.filter((cmd) => ['--debug', '--help'].includes(cmd) === false));
