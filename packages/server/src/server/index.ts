d89544dbecc6d9ecaf2b984d672785a99a7b91e5
L IN <linzb93@users.noreply.github.com>
2025-05-26 22:57:55 +0800

    feat:添加代理程序

--- packages/server/src/server/index.ts
+++ packages/server/src/server/index.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 import config from '../../../../config.json';
 import bug from './controllers/bug';
 import setting from './controllers/setting';
-
+import agent, { agentCallback } from './controllers/agent';
 const app = express();
 
 app.use(cors());
@@ -15,7 +15,9 @@ const router = Router();
 
 router.use('/bug', bug);
 router.use('/setting', setting);
+router.use('/agent', agent);
 app.use('/api', router);
+app.all('/agent', agentCallback);
 
 app.listen(config.port.production, () => {
     process.send?.('server-start');
