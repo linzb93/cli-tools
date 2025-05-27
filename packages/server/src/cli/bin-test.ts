6276ebdbff5d6b6a066651dc17beda4afdbecbbf
L IN <linzb93@users.noreply.github.com>
2025-05-27 22:07:21 +0800

    feat:添加调试模式的rule

--- packages/server/src/cli/bin-test.ts
+++ packages/server/src/cli/bin-test.ts
@@ -1,18 +1,13 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import git from './commands/git';
 // 创建命令行程序
 const program = new Command();
 
 // 设置程序基本信息
 program.version('1.0.0').description('CLI工具集合');
 
-program
-    .command('git [sub-command] [rest...]')
-    .allowUnknownOption()
-    .action((subCommand, rest, cmd) => {
-        git(subCommand, rest, cmd);
-    });
+//**** 请在这里替换需要调试的代码 ****
+
 // 解析命令行参数
 program.parse(process.argv);
 
