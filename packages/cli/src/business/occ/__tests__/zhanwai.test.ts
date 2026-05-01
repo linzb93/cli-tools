e25353c202113741ff9f0ef691bb99e0925e2fc0
L IN <linzb93@users.noreply.github.com>
2026-05-01 20:57:53 +0800

    test:补充测试用例

--- packages/cli/src/business/occ/__tests__/zhanwai.test.ts
+++ packages/cli/src/business/occ/__tests__/zhanwai.test.ts
@@ -8,7 +8,7 @@ describe('站外应用', () => {
     beforeEach(() => {
         vi.clearAllMocks();
     });
-    it('外卖宝应用获取地址', async () => {
+    it('外卖宝应用获取美团地址', async () => {
         const instance = new Wmb();
         const url = await instance.getShopUrl(instance.defaultId, { platform: 'meituan' });
         const token = instance.getToken(url);
@@ -16,9 +16,33 @@ describe('站外应用', () => {
         expect(token).toBeTypeOf('string');
         expect(token.startsWith('ey')).toBe(true);
     });
-    it('开店宝应用获取地址', async () => {
+    it('外卖宝应用获取淘宝地址', async () => {
+        const instance = new Wmb();
+        const url = await instance.getShopUrl(instance.defaultId, { platform: 'taobao' });
+        const token = instance.getToken(url);
+        expect(url).toBeTypeOf('string');
+        expect(token).toBeTypeOf('string');
+        expect(token.startsWith('ey')).toBe(true);
+    });
+    it('外卖宝应用获取京东地址', async () => {
+        const instance = new Wmb();
+        const url = await instance.getShopUrl(instance.defaultId, { platform: 'jingdong' });
+        const token = instance.getToken(url);
+        expect(url).toBeTypeOf('string');
+        expect(token).toBeTypeOf('string');
+        expect(token.startsWith('ey')).toBe(true);
+    });
+    it('开店宝应用获取美团地址', async () => {
         const instance = new Kdb();
         const url = await instance.getShopUrl(instance.defaultId, { platform: 'meituan' });
+        const token = instance.getToken(url);
+        expect(url).toBeTypeOf('string');
+        expect(token).toBeTypeOf('string');
+        expect(token.startsWith('ey')).toBe(true);
+    });
+    it('开店宝应用获取淘宝地址', async () => {
+        const instance = new Kdb();
+        const url = await instance.getShopUrl(instance.defaultId, { platform: 'taobao' });
         const token = instance.getToken(url);
         expect(url).toBeTypeOf('string');
         expect(token).toBeTypeOf('string');
